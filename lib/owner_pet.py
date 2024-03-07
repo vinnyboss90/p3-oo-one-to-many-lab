98291576e0ffdf5a598e9c89ed9ebb7dc67d7498
vinnyegon90 <vinnyegon90@gmail.com>
2024-03-07 23:05:17 +0300

    Done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,33 @@
 class Pet:
-    pass
-
+    
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
+    
+    def __init__(self, name, pet_type, owner=""):
+        self.name = name
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type=pet_type
+        else:
+            raise Exception
+        self.owner = owner
+        # Define a class variable all that stores all instances of the Pet class.
+        Pet.all.append(self)
+       
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        # return Pet.all
+        return [pets for pets in Pet.all]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        # Sort the Pet.all list by name
+        sorted_pets = sorted(Pet.all, key=lambda pet: pet.name)
+        return sorted_pets
+        
